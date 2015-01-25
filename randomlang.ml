78db4a7b59897dacff4271114d63953c0df64c73
Maxim Avanov <maxim.avanov@gmail.com>
2015-01-25 14:24:23 +0300

    Accept languages from stdin

--- randomlang.ml
+++ randomlang.ml
@@ -6,37 +6,22 @@ type language_meta = {
     weight : int;
 }
 
-(* Initialize Random so it won't use the default seed
-   self_init () initializes the generator with a random seed chosen in a
-   system-dependent way. If /dev/urandom is available on the host machine,
-   it is used to provide a highly random initial seed. Otherwise, a less
-   random seed is computed from system parameters (current time, process IDs)
-*)
-Random.self_init () ;;
 
-let () =
-    let available_languages = [
-        {
-            name = "en_US";
-            weight = 1;
-        };
-        {
-            name = "de_DE";
-            weight = 0;
-        };
-        {
-            name = "es_ES";
-            weight = 0;
-        };
-        {
-            name = "fr_FR";
-            weight = 0;
-        };
-    ] in
-    let enabled_languages = List.filter available_languages ~f:(
-        fun lang -> match lang.weight with
-                    | 0 -> false
-                    | _ -> true
+let main_handler languages () =
+    let splitter = String.lsplit2 ~on:':' in
+    let available_languages = List.filter_map
+        languages
+        ~f:(fun lang -> match splitter lang with
+            | None as n -> n
+            | Some x -> let name, weight = x in
+                        Some {name; weight=(int_of_string weight)}
+        )
+    in
+    let enabled_languages = List.filter
+        available_languages
+        ~f:(fun lang -> match lang.weight with
+                        | 0 -> false
+                        | _ -> true
         )
     in
     let languages_seq = List.fold_left
@@ -56,4 +41,40 @@ let () =
                           | None -> "en_US")
     in
     Printf.printf "%s.utf8\n" language_choice
+
+
+let cmd_spec = 
+    let open Command.Spec in
+        (* verify input parameters *)
+        step ( fun next_step languages ->
+                    (* The first m argument to the step callback is the next callback function in the chain. *)
+                    match languages with
+                    | [] -> eprintf "You must specify at least one language.\n%!";
+                            exit 1
+                    | _ -> next_step languages )
+        +> anon (sequence ("language" %: string))
+
+
+let cmd = Command.basic
+    ~summary: "Select a random lang from the provided weighed sequence"
+    ~readme: (fun () -> "Select a random lang from the provided weighed sequence")
+    cmd_spec
+    main_handler
+;;
+
+
+(* Initialize Random so it won't use the default seed
+   self_init () initializes the generator with a random seed chosen in a
+   system-dependent way. If /dev/urandom is available on the host machine,
+   it is used to provide a highly random initial seed. Otherwise, a less
+   random seed is computed from system parameters (current time, process IDs)
+*)
+Random.self_init () ;;
+
+
+let () =
+    Command.run
+        ~version: "1.1"
+        ~build_info: "by Maxim Avanov"
+        cmd
 ;;
